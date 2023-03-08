76ee63e8407563743452575510813ffb59ace544
Tamar  khuskivadze <86792404+Khuskivadze@users.noreply.github.com>
2023-03-08 19:00:26 +0400

    full page

--- js/main-File.js
+++ js/main-File.js
@@ -23,3 +23,51 @@ window.addEventListener('resize', function() {
         document.body.classList.remove("resize-animation-stopper ");
     }, 400)
 })
+
+// navigation in small screen
+const links = document.querySelectorAll('.nav-link');
+links.forEach(function(link){
+    link.addEventListener('click', function(){
+        document.body.style.overflow = "visible";
+        navDiv.classList.remove('showNav');
+        toggleBtn.firstElementChild.className = "fas fa-bars fa-fw";
+    });
+});
+
+
+// review slider 
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+let idCount = 0;
+const reviewSlide = document.querySelectorAll('.review-item');
+
+showCurrentSlide(idCount);
+function showCurrentSlide(id){
+    hideAllSlide();
+    reviewSlide.forEach(function(item, idx){
+        if(id == idx){
+            item.classList.add('activeSlide');
+        }
+    });
+}
+function hideAllSlide(){
+    reviewSlide.forEach(function(item){
+        item.classList.remove('activeSlide');
+    })
+}
+
+prevBtn.addEventListener('click', function(){
+    idCount--;
+    if(idCount < 0){
+        idCount = reviewSlide.length - 1;
+    }
+    showCurrentSlide(idCount);
+});
+nextBtn.addEventListener('click', function(){
+    idCount++;
+    if(idCount == reviewSlide.length){
+        idCount = 0;
+    }
+    showCurrentSlide(idCount);
+});
